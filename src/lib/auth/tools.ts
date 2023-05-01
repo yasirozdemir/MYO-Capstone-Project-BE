f0b37e687533ffc925be51ca5a942a2e576c8d40
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-01 17:39:15 +0300

    dotenv

--- src/lib/auth/tools.ts
+++ src/lib/auth/tools.ts
@@ -2,6 +2,7 @@ import createHttpError from "http-errors";
 import jwt from "jsonwebtoken";
 import UsersModel from "../../api/users/model";
 import { IUserDocument } from "../../interfaces/IUser";
+require("dotenv").config();
 export interface TokenPayload {
   _id: string;
   email: string;
