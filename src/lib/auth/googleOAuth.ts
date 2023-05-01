fe3ed1095fa78f6f2ee3750a67ade7d5a360369d
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-01 17:11:25 +0300

    dotenv upd

--- src/lib/auth/googleOAuth.ts
+++ src/lib/auth/googleOAuth.ts
@@ -1,13 +1,14 @@
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import UsersModel from "../../api/users/model";
 import { createAccessToken } from "./tools";
+require("dotenv").config();
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, API_DEV_URL } = process.env;
+const { CLIENT_ID, CLIENT_SECRET, API_DEV_URL } = process.env;
 
 const googleStrategy = new GoogleStrategy(
   {
-    clientID: GOOGLE_CLIENT_ID!,
-    clientSecret: GOOGLE_CLIENT_SECRET!,
+    clientID: CLIENT_ID!,
+    clientSecret: CLIENT_SECRET!,
     callbackURL: `${API_DEV_URL}/users/googleLogin`,
   },
   async (_, __, profile, passportNext) => {
