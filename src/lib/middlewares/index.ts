379f8a28eecfcd264faee4c301ab17319915cf86
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-01 09:43:53 +0300

    checkIsLiked mw

--- src/lib/middlewares/index.ts
+++ src/lib/middlewares/index.ts
@@ -3,8 +3,10 @@ import { IUserRequest } from "../auth/jwt";
 import createHttpError from "http-errors";
 import WLsModel from "../../api/watchlists/model";
 import { trigger404 } from "../../errorHandlers";
+import UsersModel from "../../api/users/model";
+import { IUser } from "../../interfaces/IUser";
 
-export const checkMember: RequestHandler = async (req, res, next) => {
+export const checkIsMemberOfWL: RequestHandler = async (req, res, next) => {
   const userID = (req as IUserRequest).user!._id;
   const WL = await WLsModel.findById(req.params.WLID);
   if (WL) {
@@ -13,6 +15,25 @@ export const checkMember: RequestHandler = async (req, res, next) => {
     } else {
       next(createHttpError(401, "You are not a member of this watchlist!"));
     }
+  } else {
+    next(trigger404("Watchlist", req.params.WLID));
+  }
+};
+
+export const checkIsLiked: RequestHandler = async (req, res, next) => {
+  const userID = (req as IUserRequest).user!._id;
+  const WL = await WLsModel.findById(req.params.WLID);
+  if (WL) {
+    const user = (await UsersModel.findById(userID)) as IUser;
+    const isLiked =
+      WL.likes.some((id) => id.toString() === userID) &&
+      user.likedWatchlists.some((id) => id === WL._id.toString());
+    if (isLiked) {
+      (req as IUserRequest).isLiked = true;
+    } else {
+      (req as IUserRequest).isLiked = false;
+    }
+    next();
   } else {
     next(trigger404("Watchlist", req.params.WLID));
   }
