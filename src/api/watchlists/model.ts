2e9111c8c55edf954b0694df83bf80382c43b043
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-01 10:08:44 +0300

    adds timestamps to WLs

--- src/api/watchlists/model.ts
+++ src/api/watchlists/model.ts
@@ -4,20 +4,24 @@ import {
   IWatchlistsModel,
 } from "../../interfaces/IWatchlist";
 
-const WatchlistSchema = new Schema({
-  name: { type: String, required: true, default: "New Watchlist" },
-  cover: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/yasirdev/image/upload/v1682762502/capstone/dev/cover_pwdaaj.png",
+const WatchlistSchema = new Schema(
+  {
+    name: { type: String, required: true, default: "New Watchlist" },
+    cover: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/yasirdev/image/upload/v1682762502/capstone/dev/cover_pwdaaj.png",
+    },
+    members: [{ type: mongoose.Types.ObjectId, ref: "user" }],
+    movies: [{ type: mongoose.Types.ObjectId, ref: "movie" }],
+    likes: [{ type: mongoose.Types.ObjectId, ref: "user" }],
   },
-  members: [{ type: mongoose.Types.ObjectId, ref: "user" }],
-  movies: [{ type: mongoose.Types.ObjectId, ref: "movie" }],
-  likes: [{ type: mongoose.Types.ObjectId, ref: "user" }],
-});
+  { timestamps: true }
+);
 
 WatchlistSchema.methods.toJSON = function () {
   const watchlist = this.toObject();
+  delete watchlist.updatedAt;
   delete watchlist.__v;
   return watchlist;
 };
