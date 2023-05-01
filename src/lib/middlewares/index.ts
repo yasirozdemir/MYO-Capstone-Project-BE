869d870d30585996ccb895e51c5326382a46aa5e
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-01 15:05:34 +0300

    check follows mw updated

--- src/lib/middlewares/index.ts
+++ src/lib/middlewares/index.ts
@@ -47,22 +47,20 @@ export const checkFollows: RequestHandler = async (req, res, next) => {
   const u2ID = req.params.userID;
   if (u1ID === u2ID) {
     next(createHttpError(400, "You cannot follow yourself!"));
-  }
-  const user2 = await UsersModel.findById(u2ID);
-  if (user2) {
-    const user1 = await UsersModel.findById(u1ID);
-    if (user1) (req as IFollowChecks).user1 = user1;
-    (req as IFollowChecks).user2 = user2;
-    // checks if user1 is following user2
-    (req as IFollowChecks).ImFollowingThem = user1!.following.some(
-      (id) => id.toString() === u2ID
-    );
-    // checks if user2 is following user1
-    (req as IFollowChecks).TheyAreFollowingMe = user2.following.some(
-      (id) => id.toString() === u1ID
-    );
-    next();
   } else {
-    next(trigger404("User", u2ID));
+    const user2 = await UsersModel.findById(u2ID);
+    if (!user2) {
+      next(createHttpError(404, `User with the ID ${u2ID} not found!`));
+    } else {
+      const user1 = await UsersModel.findById(u1ID);
+      if (user1) (req as IFollowChecks).user1 = user1;
+      (req as IFollowChecks).user2 = user2;
+      // checks if user1 is following user2
+      (req as IFollowChecks).ImFollowingThem = user1!.following.includes(u2ID);
+      // checks if user2 is following user1
+      (req as IFollowChecks).TheyAreFollowingMe =
+        user2.following.includes(u1ID);
+      next();
+    }
   }
 };
