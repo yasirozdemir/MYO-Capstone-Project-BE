77c31785fe1e45485c7fbef2cd93f20f6be8b39b
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-02 15:30:34 +0300

    deploy

--- build/lib/auth/googleOAuth.js
+++ build/lib/auth/googleOAuth.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const passport_google_oauth20_1 = require("passport-google-oauth20");
 const model_1 = __importDefault(require("../../api/users/model"));
 const tools_1 = require("./tools");
-require("dotenv").config();
+// require("dotenv").config();
 const { CLIENT_ID, CLIENT_SECRET, API_URL } = process.env;
 const googleStrategy = new passport_google_oauth20_1.Strategy({
     clientID: CLIENT_ID,
--- src/lib/auth/tools.ts
+++ src/lib/auth/tools.ts
@@ -2,7 +2,7 @@ import createHttpError from "http-errors";
 import jwt from "jsonwebtoken";
 import UsersModel from "../../api/users/model";
 import { IUserDocument } from "../../interfaces/IUser";
-require("dotenv").config();
+// require("dotenv").config();
 export interface TokenPayload {
   _id: string;
   email: string;
