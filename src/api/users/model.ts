29b8f9912bd7527049b8f04c7b7343feb2acd765
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-04 10:25:23 +0300

    error code fix

--- src/api/users/model.ts
+++ src/api/users/model.ts
@@ -53,8 +53,8 @@ UsersSchema.static("checkCredentials", async function (email, pw) {
   if (user) {
     const passwordMatch = await bcrypt.compare(pw, user.password);
     if (passwordMatch) return user;
-    else throw new createHttpError[401]("Wrong password!");
-  } else throw new createHttpError[401]("Wrong email!");
+    else throw new createHttpError[400]("Wrong password!");
+  } else throw new createHttpError[400]("Wrong email!");
 });
 
 export default model<IUserDocument, IUsersModel>("user", UsersSchema);
